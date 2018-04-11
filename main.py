8e7cb91dbf0b4410435b0c9a55fdeb3a79c47402
mcheng3 <michael.cheng65@gmail.com>
2018-04-11 14:40:18 -0400

    Update app.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, session, redirect, url_for, request, flash
+from flask import Flask, render_template, session, redirect, url_for, flash
 import random
 import os
+import requests
 
-
+key_file = open("keys.txt", "r")
+app_id = key_file.readline()
+app_key = key_file.readline()
 
 app = Flask(__name__)
 
 @app.route('/')
 def root_route():
-    print images.search("hello")[0]
     return render_template("choose.html")
 
 
 @app.route('/map')
-def root_route():
-    print images.search("hello")[0]
+def map():
     return render_template("map.html")
 
+@app.route('/coords')
+def coords():
+    r= requests.get('https://api.tfl.gov.uk/StopPoint/Type/NaptanMetroStation', data={"app_id":app_id, "app_key":app_key})
+    stations = r.json()
+    stops = []
+    for station in stations:
+        if "tube" in station["modes"] or "dlr" in station["modes"] or "tflrail" in station["modes"]:
+            stops.append(station)
+    print stops
+    return stops
 
 if __name__ == "__main__":
     app.debug = True
