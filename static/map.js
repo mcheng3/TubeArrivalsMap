f2cdc357bf76be13fb3c369e62441a5c2cdf3235
mcheng3 <michael.cheng65@gmail.com>
2018-04-17 17:37:53 -0400

    Update map.js

--- static/map.js
+++ static/map.js
@@ -20,13 +20,13 @@ function getCoords(){
 
 function drawAll(lines){
 	for(var line in lines){
-		if(line == "central") draw(lines, line, true);
+		if(line == "metropolitan") draw(lines, line, true);
 		else draw(lines, line, false);
 	}
 }
 function draw(lines, line, displayNames){
 	//console.log(lines[line]['stops']);
-	console.log('line');
+	//console.log('line');
 	/*var lines = map.selectAll("line").data(data[line]['stops']).enter().append("line");
     var x1 = data[line]['stops'][0]['lat'];
     var x2 = data[line]['stops'][0]['lon'];
@@ -35,68 +35,58 @@ function draw(lines, line, displayNames){
     	x1 = d['lat'];
     	console.log(x);
     	lines.attr("x2", function(d){
-    		return Math.floor(x1 * 1000 - 51400);
+    		return Math.floor(x1 * 1100 - 51400);
     	});
-	   return Math.floor(x * 1000 - 51400);
+	   return Math.floor(x * 1100 - 51400);
     });
     lines.attr("y1",function(d){
-	   return 1000-Math.floor(d[0]/100);
+	   return 1100-Math.floor(d[0]/100);
     });***/
     var container = d3.select("svg");
-   
-    var path_coords = [];
-    for (var i = 0; i <= lines[line]['stops'].length - 2; i++){
-    	var line_coords = [];
-    	line_coords.push(lines[line]['stops'][i]['lon']);
-    	line_coords.push(lines[line]['stops'][i]['lat']);
-    	line_coords.push(lines[line]['stops'][i+1]['lon']);
-    	line_coords.push(lines[line]['stops'][i+1]['lat']);
-    	path_coords[i] = line_coords;
-    };
+    lineCoords = lines[line]["lines"];
+    var path = "";
+    for(var i = 0; i < lineCoords.length; i++){
+        //console.log(lineCoords[i]);
+        l = JSON.parse(lineCoords[i])[0];
+        console.log(l);
+        path = path + "M " + Math.floor((l[0][0] + .65) * 1100).toString() + " " + (parseInt(container.attr("height")) - Math.floor((l[0][1] - 51.395) * 1900)).toString() + " ";
+        for(var j = 1; j < l.length; j++){
+            path = path + "L " + Math.floor((l[j][0] + .65) * 1100).toString() + " " + (parseInt(container.attr("height")) - Math.floor((l[j][1] -51.395) * 1900)).toString() + " "
+        }
+        //console.log(path);
+    }
+    console.log(path);
     //console.log(path_coords);
-    var paths = container.selectAll("."+line+"-path").data(path_coords).enter().append("line");
-     var stops = container.selectAll("."+line+"-stop").data(lines[line]['stops']).enter().append("circle");
-    paths.attr("x1",function(d){
-    	console.log(d);
-    	return Math.floor((d[0]+ .34) * 2800);
-    });
-    paths.attr("y1",function(d){
-    	return parseInt(container.attr("height")) - Math.floor(d[1] * 3600 - 51.45 * 3600);
-    });
-    paths.attr("x2",function(d){
-    	return Math.floor(d[2] * 2800 + .34 * 2800);
-    });
-    paths.attr("y2",function(d){
-    	return parseInt(container.attr("height")) - Math.floor(d[3] * 3600 - 51.45 * 3600);
-    });
-    paths.classed(line, true)
-    console.log(colors[line]);
-    paths.attr("style", "stroke:"+colors[line]+";stroke-width:3px;")
+    var paths = container.append("path");
+    paths.attr("d", path);
+    //console.log(colors[line]);
+    paths.attr("stroke", colors[line]).attr("stroke-width", "2px").attr("fill","none");
+    var stops = container.selectAll("."+line+"-stop").data(lines[line]['stops']).enter().append("circle");
     stops.attr("cy", function(d){
     	//console.log(d['lat'] * 3000 - 51.4 * 3000);
-    	return parseInt(container.attr("height")) - Math.floor(d['lat'] * 3600 - 51.45 * 3600);
+    	return parseInt(container.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900);
     });
     stops.attr("cx", function(d){
     	//console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
-    	return Math.floor(d['lon'] * 2800 + .34 * 2800);
+    	return Math.floor((d['lon'] + .65) * 1100);
     });
     if(displayNames){
 	    var names = container.selectAll("."+line+"-name").data(lines[line]['stops']).enter().append("text");
 	    names.attr("y", function(d){
 	    	//console.log(d['lat'] * 3000 - 51.4 * 3000);
-	    	return parseInt(container.attr("height")) - Math.floor(d['lat'] * 3600 - 51.45 * 3600) - 6;
+	    	return parseInt(container.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900) - 6;
 	    });
 	    names.attr("x", function(d){
 	    	//console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
-	    	return Math.floor(d['lon'] * 2800 + .34 * 2800)+10;
+	    	return Math.floor((d['lon'] + .65) * 1100)+10;
 	    });
-	names.attr("stroke","black").attr("stroke-width", "1px").attr("fill", "white");
+	names.attr("stroke","black");
 	    names.text(function(d){
-	    	return d['name'].slice(0, d['name'].indexOf('Underground', 0));
+	    	return d['name'].slice(0, d['name'].indexOf('Underground', 0))/* + " " + d['lon'].toString() + ", " + d['lat'].toString()*/;
 	    });
 	
 	}
-    stops.attr("r", 5).attr("stroke","black").attr("stroke-width","2").attr("fill","white");
+    stops.attr("r", 3).attr("stroke","black").attr("stroke-width","1.5").attr("fill","white");
 	stops.classed(line, true)
 	
 };
