51a47045947ed6c546824fb92de3e85e22d26d56
mcheng3 <michael.cheng65@gmail.com>
2018-04-18 22:15:06 -0400

    Add panning/zooming

--- static/map.js
+++ static/map.js
@@ -1,11 +1,12 @@
 
-
+var svg = d3.select("svg");
 var colors = {"bakerloo": '#B36305', 'central':'#e32017', 'circle':'#ffd300','hammersmith-city':'#f3a9bb','jubilee':'#a0a5a9','district':'#00782a',
-			  'metropolitan':'#9b0056',
-			  'northern':'#000000',
-			  'piccadilly':'#003688',
-			  'victoria': '#0098d4',
-			  'waterloo-city': '95cdba'};
+              'metropolitan':'#9b0056',
+              'northern':'#000000',
+              'piccadilly':'#003688',
+              'victoria': '#0098d4',
+          'waterloo-city': '95cdba'};
+var container = svg.append("g");
 
 function getCoords(){
     $.ajax({
@@ -19,39 +20,49 @@ function getCoords(){
 };
 
 function drawAll(lines){
-	for(var line in lines){
-		if(line == "metropolitan") draw(lines, line, false);
-		else draw(lines, line, false);
-	}
+    for(var line in lines){
+        if(line == "metropolitan") draw(lines, line, false);
+        else draw(lines, line, false);
+    }
 }
+
+function color(seconds){
+    if(seconds >= 255){
+    return "#ffffff";
+    }
+    else{
+    return "#ff" + seconds.toString(16) + seconds.toString(16);
+    }
+}
+
 function draw(lines, line, displayNames){
-	//console.log(lines[line]['stops']);
-	//console.log('line');
-	/*var lines = map.selectAll("line").data(data[line]['stops']).enter().append("line");
+    //console.log(lines[line]['stops']);
+    //console.log('line');
+    /*var lines = map.selectAll("line").data(data[line]['stops']).enter().append("line");
     var x1 = data[line]['stops'][0]['lat'];
     var x2 = data[line]['stops'][0]['lon'];
     lines.attr("x1",function(d){
-    	x = x1;
-    	x1 = d['lat'];
-    	console.log(x);
-    	lines.attr("x2", function(d){
-    		return Math.floor(x1 * 1100 - 51400);
-    	});
-	   return Math.floor(x * 1100 - 51400);
+        x = x1;
+        x1 = d['lat'];
+        console.log(x);
+        lines.attr("x2", function(d){
+            return Math.floor(x1 * 1100 - 51400);
+        });
+       return Math.floor(x * 1100 - 51400);
     });
     lines.attr("y1",function(d){
-	   return 1100-Math.floor(d[0]/100);
+       return 1100-Math.floor(d[0]/100);
     });***/
-    var container = d3.select("svg");
+ 
     lineCoords = lines[line]["lines"];
     var path = "";
     for(var i = 0; i < lineCoords.length; i++){
         //console.log(lineCoords[i]);
         l = JSON.parse(lineCoords[i])[0];
-        console.log(l);
-        path = path + "M " + Math.floor((l[0][0] + .65) * 1100).toString() + " " + (parseInt(container.attr("height")) - Math.floor((l[0][1] - 51.395) * 1900)).toString() + " ";
+        console.log(l[0][1]);
+        path = path + "M " + Math.floor((l[0][0] + .65) * 1100).toString() + " " + (parseInt(svg.attr("height")) - Math.floor((l[0][1] - 51.395) * 1900)).toString() + " ";
         for(var j = 1; j < l.length; j++){
-            path = path + "L " + Math.floor((l[j][0] + .65) * 1100).toString() + " " + (parseInt(container.attr("height")) - Math.floor((l[j][1] -51.395) * 1900)).toString() + " "
+            path = path + "L " + Math.floor((l[j][0] + .65) * 1100).toString() + " " + (parseInt(svg.attr("height")) - Math.floor((l[j][1] -51.395) * 1900)).toString() + " "
         }
         //console.log(path);
     }
@@ -63,32 +74,38 @@ function draw(lines, line, displayNames){
     paths.attr("stroke", colors[line]).attr("stroke-width", "2px").attr("fill","none");
     var stops = container.selectAll("."+line+"-stop").data(lines[line]['stops']).enter().append("circle");
     stops.attr("cy", function(d){
-    	//console.log(d['lat'] * 3000 - 51.4 * 3000);
-    	return parseInt(container.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900);
+        //console.log(d['lat'] * 3000 - 51.4 * 3000);
+        return parseInt(svg.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900);
     });
     stops.attr("cx", function(d){
-    	//console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
-    	return Math.floor((d['lon'] + .65) * 1100);
+        //console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
+        return Math.floor((d['lon'] + .65) * 1100);
     });
     if(displayNames){
-	    var names = container.selectAll("."+line+"-name").data(lines[line]['stops']).enter().append("text");
-	    names.attr("y", function(d){
-	    	//console.log(d['lat'] * 3000 - 51.4 * 3000);
-	    	return parseInt(container.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900) - 6;
-	    });
-	    names.attr("x", function(d){
-	    	//console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
-	    	return Math.floor((d['lon'] + .65) * 1100)+10;
-	    });
-	names.attr("stroke","black");
-	    names.text(function(d){
-	    	return d['name'].slice(0, d['name'].indexOf('Underground', 0))/* + " " + d['lon'].toString() + ", " + d['lat'].toString()*/;
-	    });
-	
-	}
+        var names = container.selectAll("."+line+"-name").data(lines[line]['stops']).enter().append("text");
+        names.attr("y", function(d){
+            //console.log(d['lat'] * 3000 - 51.4 * 3000);
+            return parseInt(svg.attr("height")) - Math.floor((d['lat'] - 51.395) * 1900) - 6;
+        });
+        names.attr("x", function(d){
+            //console.log(Math.floor(d['lon'] * 3000 + .35 * 3000));
+            return Math.floor((d['lon'] + .65) * 1100)+10;
+        });
+    names.attr("stroke","black");
+        names.text(function(d){
+            return d['name'].slice(0, d['name'].indexOf('Underground', 0))/* + " " + d['lon'].toString() + ", " + d['lat'].toString()*/;
+        });
+    
+    }
     stops.attr("r", 3).attr("stroke","black").attr("stroke-width","1").attr("fill","white");
-	stops.classed(line, true)
-	
+    stops.classed(line, true)
+    
+    
+    d3.select("body").attr("width",svg.attr("width")).attr("length",svg.attr("length")).style("fill", "none").style("pointer-events", "all").call(d3.zoom()
+                     .scaleExtent([1, 3])
+                     .on("zoom", function(){
+                         container.attr("transform", d3.event.transform);
+                     })).append("g");
 };
 
 getCoords()
